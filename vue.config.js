4a7b349c910e686bdff382f903882fccc92ea7a6
Guillaume Gonidou <31546068+DalSchim@users.noreply.github.com>
2023-12-12 13:38:22 +0100

    delette api -call

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,5 @@ module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
         port: 3000,
-        proxy: {
-            '^/api': {
-                target: 'http://localhost:8080',
-                changeOrigin: true
-            }
-        }
     }
 })
