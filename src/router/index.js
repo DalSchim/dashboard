f51120eef76316d000ac7c20e3a928105d6c1563
Guillaume Gonidou <31546068+DalSchim@users.noreply.github.com>
2023-12-12 14:55:48 +0100

    test 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from "@/views/auth/LoginView.vue";
 import NotView from "@/views/NotView.vue";
 import dashBoard from "@/views/Admin/DashBoard.vue";
 
 const routes = [
     {
-        path: '/ | /#/',
+        path: '/' & '/#/',
         redirect: '/login',
         children: [
             {path: '/login', name: 'login', component: Login,}
@@ -66,7 +66,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
